3755da2880d738b75e12e354580be3dbfcb4ed83
ds4dh <ds4dh.lab@gmail.com>
2022-12-09 17:36:22 +0100

    changed environment

--- environments/build_env.sh
+++ environments/build_env.sh
@@ -1,6 +1,7 @@
 source ~/miniconda3/etc/profile.d/conda.sh  # to use conda from .sh script
 conda create -y -n chempred_revision
 conda activate chempred_revision
-conda install -y gensim pandas numpy rdkit
+conda install -y gensim pandas numpy tqdm
+conda install -y selfies rdkit -c conda-forge
 pip install SmilesPE
 conda env export > environment.yml
