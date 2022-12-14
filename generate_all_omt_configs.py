95c3b10c3fe701bfda45f070b42d2af837243780
ds4dh <ds4dh.lab@gmail.com>
2022-12-14 08:45:04 +0100

    added rdkit config file generation

--- generate_all_omt_configs.py
+++ generate_all_omt_configs.py
@@ -16,22 +16,45 @@ freeze_word_vecs_dec: True
 
 
 def main():
+    # generate_data_based_config_files()
+    generate_round_trip_based_config_files()
+    print('Done!')
+
+
+def generate_data_based_config_files():
     for folder, subfolders, _ in os.walk('data'):
         if len(subfolders) == 0:
             if 'x1' in folder and 'x10' not in folder\
             and '-single' not in folder and '-noreag' not in folder:
-                build_config_folder(folder, EMBED_TYPES)
+                setup_train_and_test_configs(folder, EMBED_TYPES)
             elif 'original' not in folder:
-                build_config_folder(folder, [EMBED_TYPES[0]])
-    print('Done!')
+                setup_train_and_test_configs(folder, [EMBED_TYPES[0]])
 
 
-def build_config_folder(data_folder, embed_types_for_this_data):
-    spec_folder = os.path.join(*data_folder.split(os.path.sep)[1:])
+def generate_round_trip_based_config_files():
+    reactant_pred_folder = os.path.join('data', 'reactant-pred')
+    for folder, subfolders, _ in os.walk(reactant_pred_folder):
+        if len(subfolders) == 0:
+            spec_path = os.path.join(*folder.split(os.path.sep)[1:])
+            config_folder = create_folder(CONFIG_DIR, spec_path, 'from-scratch')
+            write_config_file(config_folder, 'test')
+
+
+def setup_train_and_test_configs(data_folder, embed_types_for_this_data):
+    spec_path = os.path.join(*data_folder.split(os.path.sep)[1:])
     for embed_type in embed_types_for_this_data:
-        config_folder = create_config_folder(spec_folder, embed_type)
+        config_folder = create_folder(CONFIG_DIR, spec_path, embed_type)
+        logs_folder = create_folder(LOGS_DIR, spec_path, embed_type)
+        os.makedirs(config_folder, exist_ok=True)
+        os.makedirs(logs_folder, exist_ok=True)
         write_config_file(config_folder, 'train')
         write_config_file(config_folder, 'test')
+
+
+def create_folder(base_dir, spec_path, embed_type):
+    folder = os.path.join(base_dir, spec_path, embed_type)
+    os.makedirs(folder, exist_ok=True)
+    return folder
 
 
 def create_config_folder(spec_folder, embed_type):
@@ -55,7 +78,7 @@ def write_config_file(config_folder, mode):
 def apply_parameters_to_config_file(to_write, config_folder):
     spec_folder, embed_type = os.path.split(config_folder)
     spec_folder = spec_folder.replace(CONFIG_DIR, '')[1:]  # no '/' at base
-    n_steps = str(int(spec_folder.split('x')[-1]) * N_BASE_STEPS)
+    n_steps = str(int(spec_folder.split('x')[-1]) * N_BASE_STEPS)  # TODO: check for -1 in test config file
     return to_write.replace('$SPEC_FOLDER', spec_folder)\
                    .replace('$EMBED_TYPE', embed_type)\
                    .replace('$N_STEPS', n_steps)
