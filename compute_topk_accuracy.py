f99856594d0e487abec2d4872aa9130e1e12c30d
albornet <alban.bornet@gmail.com>
2022-12-15 17:47:21 +0100

    continuted working on topk accuracy script

--- compute_topk_accuracy.py
+++ compute_topk_accuracy.py
@@ -1,4 +1,7 @@
-import rdkit
+import selfies as sf
+from rdkit import Chem
+# from rdkit import RDLogger
+# RDLogger.DisableLog('rdApp.*')
 
 
 ALL_KS = [1, 3, 5, 10]
@@ -18,13 +21,25 @@ def compute_model_topk_accuracy(pred_path, gold_path, k):
     all_golds = [''.join(g.strip().split()) for g in all_golds]
     n_preds_per_gold = len(all_preds) // len(all_golds)
     for i, gold in enumerate(all_golds):
-        preds = all_preds[i * n_preds_per_gold:(i + 1) * n_preds_per_gold]
-        if len(preds) > 0:
-            print()
-            print(gold)
-            [print(p) for p in preds]
+        preds = all_preds[i * n_preds_per_gold:i * n_preds_per_gold + k]
+        if 'selfies' in pred_path:
+            preds = [sf.decoder(p) for p in preds]
+            gold = sf.decoder(gold)
+        preds = [canonicalize_smiles(p) for p in preds]
+        gold = canonicalize_smiles(gold)
+        for pred in preds:
+            pred_molecules = pred.split('.')
+            gold_molecules = gold.split('.')
+            for gold_molecule in gold_molecules:
+                pass
+
+
+def canonicalize_smiles(smiles):
+    try:
+        return Chem.MolToSmiles(Chem.MolFromSmiles(smiles))
+    except:  # Boost.Python.ArgumentError
+        return smiles
 
 
 if __name__ == '__main__':
     main()
-    
