b11e3f46eff018f3b24995a508463a0a3681e597
albornet <alban.bornet@gmail.com>
2023-01-16 15:09:38 +0100

    make sure that tests are run before roundtrip predictions

--- test_and_roundtrip_all_models.py
+++ test_and_roundtrip_all_models.py
@@ -1,15 +1,37 @@
 import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--test', action='store_true')
+parser.add_argument('-r', '--roundtrip', action='store_true')
+args = parser.parse_args()
 
 
 TEST_SCRIPT = 'python open-nmt/translate.py'
 CONFIGS_DIR = os.path.join('.', 'configs')
 MODES = ['test', 'test-50k', 'roundtrip', 'roundtrip-50k']
 FOLDS = [1, 2, 5, 10, 20]
+DO_TEST = args.test
+DO_ROUNDTRIP = args.roundtrip
 
 
 def main():
-    for mode in MODES:
+    if DO_TEST: modes_to_run = [m for m in MODES if 'test' in m]
+    if DO_ROUNDTRIP: modes_to_run = [m for m in MODES if 'roundtrip' in m]
+    if not DO_TEST and not DO_ROUNDTRIP:
+        raise ValueError('Use this script with one of the following argument:'\
+                         '\n-t to generate predictions for test tasks'\
+                         '\n-r to generate predictions for roundtrip tasks '\
+                         '(only after tests are run!)')
+    if DO_TEST and DO_ROUNDTRIP:
+        raise ValueError('You should use this script with only one argument.'\
+                         '\n-t to generate predictions for test tasks'\
+                         '\n-r to generate prediction for roundtrip tasks'\
+                         '\nIt is first required to run test predictions,'\
+                         '\nthen write roundtrip config scripts,'\
+                         '\nand only then generate roundtrip predictions.')
+    for mode in modes_to_run:
         generate_predictions_with_all_models(mode=mode)
+        print('Predictions generated for %s tasks!' % mode)
 
 
 def generate_predictions_with_all_models(mode):
@@ -17,7 +39,7 @@ def generate_predictions_with_all_models(mode):
         if '%s.yml' % mode in files:
             config_path = os.path.join(folder, '%s.yml' % mode)
             print('Starting %s' % config_path)
-            os.system('%s -config %s' % (TEST_SCRIPT, config_path))
+            # os.system('%s -config %s' % (TEST_SCRIPT, config_path))
             print('Done %s' % config_path)
 
 
