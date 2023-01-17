003014af27a4cc4e1e56174e6772468a115f489b
albornet <alban.bornet@gmail.com>
2023-01-17 17:13:00 +0100

    no share vocab test for reagent prediction

--- write_train_configs.py
+++ write_train_configs.py
@@ -16,6 +16,9 @@ word_vec_size: 256
 freeze_word_vecs_enc: True
 freeze_word_vecs_dec: True
 """ % os.path.sep
+NOT_SHARE_VOCAB_TEXT = """tgt_vocab: $DATA_FOLDER$SEPtgt_vocab.vocab
+share_vocab: False"""
+SHARE_VOCAB_TEXT = """share_vocab: True"""
 
 
 def main():
@@ -41,15 +44,19 @@ def setup_train_configs(data_folder, embed_types_for_this_data):
 
 def write_train_config_file(config_folder, data_folder, logs_folder):
     to_write = open(BASE_CONFIG_PATH, 'r').read()
+    vocab_text = SHARE_VOCAB_TEXT
+    if 'reagent-pred' in config_folder:
+        vocab_text = NOT_SHARE_VOCAB_TEXT
     if 'pre-trained' in config_folder: to_write += W2V_TEXT
     fold_flag = os.path.basename(data_folder).split('x')[-1]
     n_steps_train_max = str(int(fold_flag) * N_BASE_TRAIN_STEPS)
     n_steps_for_valid = str(int(fold_flag) * N_BASE_VALID_STEPS)
     config_path = os.path.join(config_folder, 'train.yml')
     with open(config_path, 'w') as f:
-        f.writelines(to_write.replace('$LOGS_FOLDER', logs_folder)\
-                             .replace('$DATA_FOLDER', data_folder)\
-                             .replace('$N_STEPS_TRAIN_MAX', n_steps_train_max)\
+        f.writelines(to_write.replace('$VOCAB_TEXT', vocab_text)
+                             .replace('$LOGS_FOLDER', logs_folder)
+                             .replace('$DATA_FOLDER', data_folder)
+                             .replace('$N_STEPS_TRAIN_MAX', n_steps_train_max)
                              .replace('$N_STEPS_FOR_VALID', n_steps_for_valid)
                              .replace('$SEP', os.path.sep))
 
